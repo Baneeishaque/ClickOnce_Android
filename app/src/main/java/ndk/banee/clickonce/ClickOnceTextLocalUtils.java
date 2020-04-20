8c4b099671fa0fa1a73f2be80f23fbe5ad10605c
Banee Ishaque K <baneeishaque@gmail.com>
2020-04-20 05:00:02 +0000

    Updated submodule Android_Common_Utils_API_16

---
+++ app/src/main/java/ndk/banee/clickonce/ClickOnceTextLocalUtils.java
@@ -0,0 +1,11 @@
+package ndk.banee.clickonce;
+
+import ndk.utils_j2se.TextLocalUtilsWrapper;
+
+public class ClickOnceTextUtils extends TextLocalUtilsWrapper {
+
+    public ClickOnceTextUtils() {
+
+        super("rmYLzX2Y7sY-rdPqOOJotr67rvxUSU9TfgvRzEkofe");
+    }
+}
